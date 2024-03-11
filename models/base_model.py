0668508c24c16fa9ce0b8646b0a1b82c652f0ecc
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-11 16:13:58 +0300

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -8,3 +8,13 @@ class BaseModel():
     updated_at = datetime.datetime.now()
     def __str__(self):
             return self.id
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
+
